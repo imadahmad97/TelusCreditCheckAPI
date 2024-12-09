2685c66dc77522fe770f6b6639fa2d0151dee647
Imad Ahmad <imad.ahmad@telus.com>
2024-12-09 11:07:10 -0800

    successfully parsing dates

--- app/creditcarduser.py
+++ app/creditcarduser.py
@@ -1,4 +1,3 @@
-from fastapi import FastAPI
 from pydantic import BaseModel
 import datetime
 
@@ -6,13 +5,22 @@ import datetime
 class CreditCardUser(BaseModel):
     first_name: str
     last_name: str
-    # date_of_birth: datetime.date
+    date_of_birth: str | datetime.date
     is_existing_customer: bool
     credit_card_number: str
-    # expiration_date: str
+    expiration_date: str | datetime.date
     cvv: str
     credit_card_issuer: str
 
+    def parse_dates(self):
+        self.date_of_birth = datetime.datetime.strptime(
+            self.date_of_birth, "%Y-%m-%d"
+        ).date()
+        expiration_year, expiration_month = map(int, self.expiration_date.split("-"))
+        print(expiration_month)
+        self.expiration_date = datetime.date(expiration_year, expiration_month, 1)
+
     def create_credit_card_user(self):
-        print(f"User: {self.first_name} {self.last_name}")
+        print(self.expiration_date)
+        self.parse_dates()
         return self
